6dc816d5ed9266c5e420ff97a5d3e080ea5590a6
GedionT <teshomegedion@gmail.com>
2022-04-11 12:48:26 +0300

    code clean up

--- index.js
+++ index.js
@@ -1,27 +1,62 @@
-require('dotenv').config()
-console.log(process.env)
-console.log(process.env.BOT_TOKKEN0,process.env.BOT_TOKKEN1)
-const { Telegraf , session, Scenes:{Stage, WizardScene} } = require('telegraf')
-const {keyboard} = require('./utils/utils')
-// console.log(process.env.BOTAPI+":"+process.env.BOTAPI_CHARARTER)
-const bot = new Telegraf(process.env.BOT_TOKKEN0+":"+process.env.BOT_TOKKEN1)
+require('dotenv').config();
 
-const {finished,voiceHandler,re_recordVoice,sendVoice,counter,startDonateHandler,Decition }= require('./scene/DonateScene')
-const DonateScene = new WizardScene('DonateVoice',counter,startDonateHandler,voiceHandler,sendVoice,re_recordVoice,finished,Decition)
-const stage =  new Stage([DonateScene])
-stage.hears('exit',ctx=>ctx.scene.leave())
+const {
+  Telegraf,
+  session,
+  Scenes: { Stage, WizardScene },
+} = require('telegraf');
 
-bot.use(session(), stage.middleware())
-bot.command('/start', ctx => 
-ctx.reply("Wellcome To Amharic Data corps collecter",keyboard)
-)
+const { keyboard } = require('./utils/utils');
 
-bot.hears('Donate',ctx=>ctx.scene.enter('DonateVoice'))
-bot.hears('Verify',ctx=>
-ctx.reply("This Feature is will be fuctional when enough amount of data is collected Please keep Donating your voice.",keyboard))
-bot.hears('About Us',ctx=>{
-  ctx.reply("My Name is Dawud Abdulkerim , Am From Ethiopia , There is almost no amharic data(audio) corpes, this project is to contribute to solve this issue.",keyboard)
-})
-bot.hears('Statistics',ctx=>{ctx.reply("None for now, In Futer it will be filled with Data",keyboard)})
-bot.command('/info',ctx => ctx.scene.enter('infoScene'))
-bot.launch()
+const bot = new Telegraf(process.env.BOT_TOKEN0 + ':' + process.env.BOT_TOKEN1);
+
+const {
+  finished,
+  voiceHandler,
+  re_recordVoice,
+  sendVoice,
+  counter,
+  startDonateHandler,
+  Diction,
+} = require('./scene/DonateScene');
+
+const DonateScene = new WizardScene(
+  'DonateVoice',
+  counter,
+  startDonateHandler,
+  voiceHandler,
+  sendVoice,
+  re_recordVoice,
+  finished,
+  Diction
+);
+
+const stage = new Stage([DonateScene]);
+stage.hears('exit', (ctx) => ctx.scene.leave());
+
+bot.use(session(), stage.middleware());
+bot.command('/start', (ctx) =>
+  ctx.reply('Welcome To Lesan Amharic Data Corpus Sourcer', keyboard)
+);
+
+bot.hears('Donate', (ctx) => ctx.scene.enter('DonateVoice'));
+bot.hears('Verify', (ctx) =>
+  ctx.reply(
+    'This feature will be functional when enough amount of data is collected. Please keep Donating your voice, Thank You!',
+    keyboard
+  )
+);
+bot.hears('About Us', (ctx) => {
+  ctx.reply(
+    "This is a project built on top of Dawud's data collection bot, to ease the process of data collection for Lesan.Studio project",
+    keyboard
+  );
+});
+bot.hears('Statistics', (ctx) => {
+  ctx.reply(
+    'As the project progresses, current stats will start to show up here. Hang tight :)',
+    keyboard
+  );
+});
+bot.command('/info', (ctx) => ctx.scene.enter('infoScene'));
+bot.launch();
